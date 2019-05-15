df2fa433ad4cce7a95f8a957638dd77021c0fb77
Vasily Yakovenko <bitnikvasya@mail.ru>
2019-05-15 16:17:30 +0300

    Правки

--- source/js/modal.js
+++ source/js/modal.js
@@ -15,7 +15,7 @@
 
   var link = document.querySelectorAll(".modal-open");
   var popup = document.querySelector(".modal");
-  var close = popup.querySelector(".modal-close");
+  var close = popup.querySelector(".modal__close");
   modalButtonArray = Array.prototype.slice.call(link);
 
   modalButtonArray.forEach(function (el) {
@@ -26,16 +26,22 @@
   });
 
 
-window.addEventListener("keydown", function (a) {
-  a.preventDefault();
-  if (a.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show")
-    }
-  }
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+evt.preventDefault();
+if (popup.classList.contains("modal-show")) {
+popup.classList.remove("modal-show");
+}
+}
 });
 
-close.addEventListener("click", function (a) {
-  a.preventDefault();
-  popup.classList.remove("modal-show")
+
+
+
+
+
+
+close.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.remove("modal-show");
 });
